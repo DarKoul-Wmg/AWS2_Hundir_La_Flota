1db348db4e08647c1c610f9d0c2bacf504df2449
jpachonguerra <javier.pachon.guerra@gmail.com>
2024-10-11 18:36:02 +0200

    test

--- pages/index.php
+++ pages/index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Troba la petxina</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 <body id="index">
-    
+    <p class="title">Hundir la Flota</p>
+    <div>
+        
 </body>
 </html>
