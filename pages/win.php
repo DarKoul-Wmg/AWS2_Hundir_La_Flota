5c1de5d7950c4fc3045f4093536f4011d6937086
Marta Arevalo <martaarevaloprieto@gmail.com>
2024-10-11 17:10:11 +0200

    añadido la implementación de las páginas

---
+++ pages/win.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Troba la petxina</title>
+</head>
+<body id="win">
+    
+</body>
+</html>
