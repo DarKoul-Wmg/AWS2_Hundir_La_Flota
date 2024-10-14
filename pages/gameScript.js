751491d0735e27942fc27bd03327dc6871e65645
jpachonguerra <javier.pachon.guerra@gmail.com>
2024-10-14 19:37:27 +0200

    create branch

---
+++ pages/gameScript.js
@@ -0,0 +1,33 @@
+const livePoints = 0;
+const lastHit = false;
+// 
+
+function calculatePoints(){
+
+}
+
+function test(){
+
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+
+
+  function startTime2() {
+    
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
