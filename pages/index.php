85ec46a26ec45a27c6f628744c43ef94765bbcb5
jpachonguerra <javier.pachon.guerra@gmail.com>
2024-10-14 15:44:19 +0200

    landing page CSS semi finished, just swap the cover placeholder

--- pages/index.php
+++ pages/index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
 <html lang="ca">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Troba la petxina</title>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
 <body id="index" class="landingPageBody">
     <p class="landingPageTitle">Troba la petxina</p>
-    <img class ="landingPageCover" src="../sources/coverPlaceholder.png" alt="Portada del joc"/>
+    <img class="landingPageCover" src="../sources/coverPlaceholder.png" alt="Portada del joc" />
     <p class="landingPageDescription">
         Troba totes les petxines com més aviat millor!
     </p>
-    <a href="game.php">
-        <button class="landingPageNewGameButton">Nova Partida</button>
-    </a>
-    <a href="ranking.php">
-        <button class="landingPageRankingButton">Ranking</button>
-    </a>
+    <div class="landingPageCenterButtons">
+        <a href="game.php">
+            <button type="button" class="landingPageNewGameButton">Nova Partida</button>
+        </a>
+        <a href="ranking.php">
+            <button type="button" class="landingPageRankingButton">Ranking</button>
+        </a>
+    </div>
 </body>
+
 </html>
