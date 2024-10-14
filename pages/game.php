97241b322a949a14bffb224af01fed1638eea048
DarKoul-Wmg <williammg0904@gmail.com>
2024-10-14 19:17:21 +0200

    game.php tablero y generación de celdas con id

--- pages/game.php
+++ pages/game.php
@@ -5,38 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
 	<script src="game.js"></script>
+
     <title>Troba la petxina</title>
 </head>
 <body id="game">
-	
     <?php
-        $filas=10;
-        $columnas=10;
+        $filas=11;
+        $columnas=11;
     ?>
 	<table>
 		<?php
-			for($i = 0; $i <= $filas; $i++){
+			for($i = 0; $i < $filas; $i++){
 				echo "<tr>";
-				for($j = 0; $j <= $columnas; $j++){
+				for($j = 0; $j < $columnas; $j++){
 					 # Crear un ID único para cada celda
 					 $id = "cell_" . $i . "_" . $j;
 					# En el caso de que sea el puesto superior izquierda no pinte nada
 					if($j==0 && $i==0){
-						echo "<td> </td>";
+						echo "<td class='empty'> </td>";
 					}
 					# En el caso de que sea la primera columna
 					else if($j==0 && $i>=1){
-						echo "<td> $i </td>";
+						echo "<td class='number'> $i </td>";
 					}
 					# En el caso de que sea la primera fila 
 					else if($j>=1 && $i==0){
 						$chrx = chr(64 + $j);
-						echo "<td> $chrx </td>";
-				
+						echo "<td class='letter'> $chrx </td>";
 					}
 					# Para las demás
 					else{
-						echo "<td>~</td>";
+						echo "<td id='$id'>~</td>";
 					}
 				}
 				echo "</tr>";
