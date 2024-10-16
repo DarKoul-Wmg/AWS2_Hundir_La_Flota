39e862d411587dd6c31e4be27bc06ca335765073
jpachonguerra <javier.pachon.guerra@gmail.com>
2024-10-16 20:19:25 +0200

    ranking page test

--- pages/ranking.php
+++ pages/ranking.php
@@ -6,6 +6,36 @@
     <title>Troba la petxina</title>
 </head>
 <body id="ranking">
-    
+<?php
+$filePath="../sources/ranking.txt";
+$linecount = 0;
+$file = fopen($filePath, "r");
+    echo'<table>';
+while(!feof($file)){
+  $line = fgets($file);
+  $linecount++;
+  while($linecount < 26){
+    $values = explode(',',$line);
+    echo'<tr>
+            <td>',$values[0],'</td>
+            <td>',$values[1],'</td>
+            <td>',$values[2],'</td>
+        </tr>
+    ';
+  } ;
+}
+if ($linecount>26) {
+    # paginador
+    $numberOfPages = $linecount / 25;
+    $numberOfPages = ceil($numberOfPages);
+    echo'
+        
+    ';
+}
+echo'</table>';
+fclose($file);
+
+
+?>
 </body>
 </html>
