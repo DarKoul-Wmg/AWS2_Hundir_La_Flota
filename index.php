84a3b66ba78d9ef2908fa2fefec9b11b46546133
William Molina <williammg0904@gmail.com>
2024-10-26 20:28:37 +0200

    ranking ordenado y highlight funcional

--- index.php
+++ index.php
@@ -67,7 +67,9 @@
         <?php
     // Crear sesión y guardar el nombre del jugador en una variable de sesión
     session_start();
+    session_unset(); // Eliminar todas las variables de sesión
 
+    
     if(isset($_POST["playerName"])){ //si tenemos nombre registrado, mostrar botones de juego activos
         $_SESSION["playerName"] = $_POST["playerName"];
 
